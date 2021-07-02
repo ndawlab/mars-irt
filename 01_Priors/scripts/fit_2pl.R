5d7135ae10a5b17eae76b171be69f6ae87cc78dc
szorowi1 <szorowi1@gmail.com>
2021-07-02 15:09:57 -0400

    first commit

---
+++ 01_Priors/scripts/fit_2pl.R
@@ -0,0 +1,33 @@
+library(rstan)
+options(mc.cores = parallel::detectCores())
+
+## Load data.
+data = read.csv('data/study_02.csv')
+
+## Define item.
+data$item = sub("_.*","",data$Puzzle)
+
+## Define data (item accuracy).
+Y = as.integer(data$Accuracy)
+
+## Define mappings. 
+J = as.integer(as.factor(data$sub))     # subject-to-observation mapping
+K = as.integer(data$item)               # item-to-observation mapping
+
+## Define metadata.
+N = nrow(data)
+
+## Assemble data for Stan.
+irt_dat = list(N=N, J=J, K=K, Y=Y)
+
+## Fit Stan Model.
+fit <- stan(file = 'stan_models/2pl.stan', data = irt_dat,
+            chains=4, iter=2000, warmup=1000, thin=1, seed=0)
+
+## Extract samples.
+samples = as.data.frame(fit)
+write.csv(samples, '2pl_samples.csv', row.names=FALSE)
+
+## Extract summary.
+summary = summary(fit)$summary
+write.csv(summary, '2pl_samples_summary.csv')
---
+++ literature.md
@@ -0,0 +1,3 @@
+https://doi.org/10.1098/rsos.190232
+https://doi.org/10.1177/0734282920930923
+https://doi.org/10.3390/jintelligence8010005
