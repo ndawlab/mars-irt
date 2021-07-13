d1781abfd6ee5516875eac5a9ab8eab0194b7548
szorowi1 <szorowi1@gmail.com>
2021-07-13 14:52:01 -0400

    pretty happy with simulations

--- 02_Design/scripts/simulate_irt.py
+++ 02_Design/scripts/simulate_irt.py
@@ -3,6 +3,7 @@ import numpy as np
 from os.path import dirname
 from cmdstanpy import CmdStanModel
 from pandas import DataFrame, concat
+from scipy.stats import norm
 ROOT_DIR = dirname(dirname(os.path.realpath(__file__)))
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -11,10 +12,12 @@ ROOT_DIR = dirname(dirname(os.path.realpath(__file__)))
 
 ## I/O parameters.
 stan_model = '2pl'
+seed = int(sys.argv[1])
 
 ## Simulation parameters.
-fout = sys.argv[1]
-seed = int(sys.argv[2])
+n_subj  = 600                # number of total subjects
+n_item  = 30                 # number of total items
+n_split = 2                  # number of item sets
 
 ## Sampling parameters.
 iter_warmup   = 2000
@@ -30,25 +33,35 @@ np.random.seed(seed)
 
 def inv_logit(x):
     return 1 / (1 + np.exp(-x))
-
-## Define task design.
-design = np.atleast_2d(np.arange(15))
+    
+## Define item order.
+ix = np.arange(n_item).reshape(n_split, n_item // n_split, order='F')
+K = np.zeros((n_subj, n_item // n_split), dtype=int)
+for i in range(n_subj): 
+    ix = np.apply_along_axis(np.random.permutation, 0, ix)
+    K[i] = ix[0]
     
 ## Define metadata.
-n_samp = 300
-    
-## Define mappings.
-J, K = [], []
-for i in np.arange(n_samp):
-    J.append(np.repeat(i, design.shape[-1]))
-    K.append(design[i % design.shape[0]])
-J = np.concatenate(J); K = np.concatenate(K)
+K = K.flatten()
+J = np.repeat(np.arange(n_subj), n_item // n_split)
 N = J.size
-    
-## Generate latent parameters.
-theta = np.random.normal(0, 1, n_samp)
-beta  = np.random.normal(0, 1, K.max()+1)
-alpha = np.exp(np.random.normal(0, 0.33, K.max()+1))
+
+## Generate subject abilities.
+theta = np.random.normal(0, 1, n_subj)
+
+## Iteratively sample item parameters.
+bins = norm.ppf(np.linspace(0.005,0.995,10))
+cov = [[1.50,0.00],[0.00,0.25]]
+
+while True:
+    beta, alpha = np.random.multivariate_normal(np.zeros(2), cov, n_item).T
+    counts = np.bincount(np.digitize(beta, bins), minlength=bins.size + 1)[1:-1]
+    if counts.min() >= 3: break
+    break
+        
+## Sort parameters.
+alpha = np.exp(alpha[np.argsort(beta)])
+beta  = beta[np.argsort(beta)]
 
 ## Generate data.
 mu = inv_logit(alpha[K] * (theta[J] - beta[K]))
@@ -65,38 +78,77 @@ dd = dict(N=N, J=J+1, K=K+1, Y=Y)
 StanModel = CmdStanModel(stan_file=os.path.join('stan_models',f'{stan_model}.stan'))
 
 ## Fit Stan model.
-StanFit = StanModel.sample(data=dd, chains=chains, iter_warmup=iter_warmup, iter_sampling=iter_sampling, thin=thin, parallel_chains=parallel_chains, seed=0, show_progress=True)
+StanFit = StanModel.sample(data=dd, chains=chains, iter_warmup=iter_warmup, iter_sampling=iter_sampling, 
+                           thin=thin, parallel_chains=parallel_chains, seed=0, show_progress=True)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-### Save data.
+### Select items.
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+## Define IRT functions.
+def info_2pl(theta, beta, alpha):
+    p = inv_logit(alpha * (theta - beta))
+    return alpha**2 * p * (1-p)
+
+## Define templates.
+x = np.linspace(-2,2,1001)
+templates = np.row_stack([
+    np.ones_like(x),
+    norm(0,1).pdf(x)
+])
 
 ## Extract samples.
 samples = StanFit.draws_pd()
+theta_hat = samples.filter(regex='theta').median().values
+beta_hat  = samples.filter(regex='beta').median().values
+alpha_hat = samples.filter(regex='alpha').median().values
 
-## Construct DataFrame. 
-data = concat([
-    DataFrame(dict(
-        param=np.repeat('theta',theta.size), 
-        obs=theta, 
-        pred=samples.filter(regex='theta').mean().values
-    )),
-    DataFrame(dict(
-        param=np.repeat('beta',beta.size), 
-        obs=beta, 
-        pred=samples.filter(regex='beta').mean().values
-    )),
-    DataFrame(dict(
-        param=np.repeat('alpha',alpha.size), 
-        obs=alpha, 
-        pred=samples.filter(regex='alpha').mean().values
-    )),
+## Compute item information.
+info_true = np.zeros((n_item, x.size))
+info_pred = info_true.copy()
+
+for i in range(n_item): 
+    info_true[i] = info_2pl(x, beta[i], alpha[i])
+    info_pred[i] = info_2pl(x, beta_hat[i], alpha_hat[i])
+
+## Greedy item selection (ground truth).
+ground = np.zeros([templates.shape[0], 9], dtype=int)
+for i, template in enumerate(templates):    
+    for j in range(ground.shape[-1]):
+        ix = np.setdiff1d(np.arange(n_item), ground[i,:j])
+        tif = (info_true[ground[i,:j]].sum(axis=0) + info_true[ix]) @ template
+        ground[i,j] = ix[np.argmax(tif)]
+        
+## Greedy item selection (chosen).
+chosen = np.zeros([templates.shape[0], 9], dtype=int)
+for i, template in enumerate(templates):    
+    for j in range(chosen.shape[-1]):
+        ix = np.setdiff1d(np.arange(n_item), chosen[i,:j])
+        tif = (info_pred[chosen[i,:j]].sum(axis=0) + info_pred[ix]) @ template
+        chosen[i,j] = ix[np.argmax(tif)]
+        
+## Greedy item selection (worst).
+worst = np.zeros([templates.shape[0], 9], dtype=int)
+for i, template in enumerate(templates):    
+    for j in range(worst.shape[-1]):
+        ix = np.setdiff1d(np.arange(n_item), worst[i,:j])
+        tif = (info_pred[worst[i,:j]].sum(axis=0) + info_pred[ix]) @ template
+        worst[i,j] = ix[np.argmin(tif)]
+        
+## Random item selection.
+random = np.row_stack([
+    np.random.choice(np.arange(n_item), 9, replace=False),
+    np.random.choice(np.arange(n_item), 9, replace=False)
 ])
 
-## Append metadata.
-data.insert(0, 'n_samp', J.max()+1)
-data.insert(1, 'n_item', K.max()+1)
-data.insert(2, 'seed', seed)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+### Save.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
-## Save.
-data.to_csv(os.path.join('stan_results', f'%s_s%0.3d.csv' %(fout, seed)), index=False)
+## Define fout.
+fout = os.path.join(ROOT_DIR, 'stan_results', '%s_n%s_s%0.3d.npz' %(stan_model, n_split, seed))
+
+## Save data.
+np.savez_compressed(fout, theta=theta, theta_hat=theta_hat, beta=beta, beta_hat=beta_hat,
+                    alpha=alpha, alpha_hat=alpha_hat, templates=templates, info=info_true,
+                    ground=ground, chosen=chosen, random=random, worst=worst)
--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # mars-irt
 
 ## TO-DO
-- finalize sampling approach: totally random parameters or semi-random parameters?
-- run models on cluster + make analysis plots, look at general range of items selected
 - select final items to run
 - decide on sampling strategy for nivturk
