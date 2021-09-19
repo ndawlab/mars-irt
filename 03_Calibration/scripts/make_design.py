46c3ce907a05eb5947e530049fef1c93ac9c330f
szorowi1 <szorowi1@gmail.com>
2021-09-19 18:07:35 -0400

    add initial dlt model

--- 03_Calibration/scripts/make_design.py
+++ 03_Calibration/scripts/make_design.py
@@ -5,103 +5,86 @@ from pandas import read_csv
 ROOT_DIR = dirname(dirname(os.path.realpath(__file__)))
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-### Define parameters.
+### Load and prepare data.
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
-## Model parameters.
-design = int(sys.argv[1])
+## Load data.
+data = read_csv(os.path.join(ROOT_DIR, 'data', 'data.csv'))
+metadata = read_csv(os.path.join(ROOT_DIR, 'data', 'metadata.tsv'), sep='\t')
+
+## Apply rejections.
+reject = read_csv(os.path.join(ROOT_DIR, 'data', 'reject.csv'))
+data = data.loc[data.subject.isin(reject.query('reject==0').subject)]
+metadata = metadata.loc[metadata.subject.isin(reject.query('reject==0').subject)]
+
+## Drop missing data.
+data = data.dropna()
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-### Load and prepare item feature matrix.
+### Construct subject feature matrix.
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
-## Load item features.
-features = read_csv(os.path.join(ROOT_DIR, 'data', 'features.csv'), index_col='item')
+## Prepare gender variable.
+gender = {'Male':-1,'Female':1,'Other':0,'Rather not say':0}
+metadata['gender'] = metadata['gender-categorical'].replace(gender)
 
-## Design 0: intercept only.
-if design == 0:
-    
-    ## Define intercept.
-    features['x0'] = 1
-    
-## Design 1: number of total rules.
-elif design == 1:
-    
-    ## Define intercept.
-    features['x0'] = 1
-    
-    ## Define rule-based regressors.
-    features['x1'] = np.where(features.filter(regex='f[1-4]') > 0, 1, 0).sum(axis=1)
-        
-## Design 2: number of total rules (by type).
-elif design == 2:
-    
-    ## Define intercept.
-    features['x0'] = 1
-    
-    ## Define rule-based regressors.
-    features['x1'] = np.where(features.filter(regex='f[1-4]') == 1, 1, 0).sum(axis=1)
-    features['x2'] = np.where(features.filter(regex='f[1-4]') == 2, 1, 0).sum(axis=1)
-    
-## Design 3: number of total rules (by feature).
-elif design == 3:
-    
-    ## Define intercept.
-    features['x0'] = 1
-    
-    ## Define rule-based regressors.
-    for i, col in enumerate(['color$','shape$','tri$','pos_[r,c]','size$']):
-        features[f'x{i+1}'] = np.where(features.filter(regex=col) > 0, 1, 0).sum(axis=1)
-    
-## Design 4: number of total rules (by type & feature).
-elif design == 4:
-    
-    ## Define intercept.
-    features['x0'] = 1
-    
-    ## Define rule-based regressors.
-    for i, col in enumerate(['color$','shape$','tri$','pos_[r,c]','size$']):
-        features[f'x{2*i+1}'] = np.where(features.filter(regex=col) == 1, 1, 0).sum(axis=1)
-        features[f'x{2*i+2}'] = np.where(features.filter(regex=col) == 2, 1, 0).sum(axis=1)
-    
-## Design 5: number of total rules (by type & feature) + distractor.
-elif design == 5:
-    
-    ## Define intercept.
-    features['x0'] = 1
-    
-    ## Define rule-based regressors.
-    for i, col in enumerate(['color$','shape$','tri$','pos_[r,c]','size$']):
-        features[f'x{2*i+1}'] = np.where(features.filter(regex=col) == 1, 1, 0).sum(axis=1)
-        features[f'x{2*i+2}'] = np.where(features.filter(regex=col) == 2, 1, 0).sum(axis=1)
-        
-    ## Define distractor-based regressors.
-    features['x11'] = np.where(features.distractor == 'md', 1, 0) 
-    
-## Design 6: number of total rules (by type & feature) + distractor + perceptual elements.
-elif design == 6:
-    
-    ## Define intercept.
-    features['x0'] = 1
-    
-    ## Define rule-based regressors.
-    for i, col in enumerate(['color$','shape$','tri$','pos_[r,c]','size$']):
-        features[f'x{2*i+1}'] = np.where(features.filter(regex=col) == 1, 1, 0).sum(axis=1)
-        features[f'x{2*i+2}'] = np.where(features.filter(regex=col) == 2, 1, 0).sum(axis=1)
-        
-    ## Define distractor-based regressors.
-    features['x11'] = np.where(features.distractor == 'md', 1, 0)
-    
-    ## Define percept-based regressors.
-    for i, j in enumerate(range(2,10)): 
-        features[f'x{12+i}'] = np.where(features.shape_subset == j, 1, 0)
-    
-else:
-    
-    raise ValueError(f'design type "{design}" not available.')
-    
-## Extract regressors.
-X = features[features.filter(regex='x[0-9]').columns].values
+## Prepare age variable.
+metadata['age'] = metadata.age.fillna(metadata.age.mean())
 
-## Save.
-np.savetxt(os.path.join(ROOT_DIR, 'designs', f'x{design}.txt'), X, fmt='%0.0f', delimiter=',')
+## Standardize RT regressors.
+zscore = lambda x: (x - np.nanmean(x)) / np.nanstd(x)
+data['y'] = zscore(np.log(data.rt))
+data['x0'] = np.ones_like(data['y'])
+data['x1'] = zscore(data.groupby('item_id').accuracy.transform(lambda x: 1 - np.mean(x)))
+
+## Prepare RT variables.
+regression = lambda df: np.linalg.lstsq(df[['x0','x1']], df['y'], rcond=-1)[0]
+b0, b1 = np.row_stack(data.groupby('subject').apply(regression)).T
+metadata['rt_0'] = b0
+metadata['rt_1'] = b1
+
+## Define subject feature matrix.
+X1 = metadata[['age','gender','rt_0','rt_1']].copy()
+X1.to_csv(os.path.join(ROOT_DIR, 'designs', f'X1.csv'), index=False)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+### Construct item feature matrix.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+## Load features DataFrame.
+features = read_csv(os.path.join(ROOT_DIR, 'designs', 'features.csv'))
+
+## Reduce to item / distractor type. 
+features = features.groupby(['item','distractor']).mean().reset_index()
+features = features.sort_values(['item','distractor']).reset_index(drop=True)
+
+## Prepare intercept variable.
+features['intercept'] = 1
+
+## Prepare rules variable.
+features['n_rules'] = np.where(features.filter(regex='f[1-3]'), 1, 0).sum(axis=1)
+
+## Prepare distractor variable.
+features['distractor'] = features['distractor'].replace({'md':1, 'pd':0})
+
+## Define item feature matrix.
+X2 = features[['intercept','n_features','n_rules','distractor']].astype(int).copy()
+X2.to_csv(os.path.join(ROOT_DIR, 'designs', f'X2.csv'), index=False)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+### Construct distractor feature matrix.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+## Load distractors DataFrame.
+distractors = read_csv(os.path.join(ROOT_DIR, 'designs', 'distractors.csv'))
+
+## Reduce to item / distractor type. 
+distractors = distractors.sort_values(['item','distractor']).reset_index(drop=True)
+distractors = distractors.set_index(['item','distractor'])
+
+## Prepare relative distractor variables.
+distractors = distractors.apply(lambda x: x - x.min(), axis=1)
+
+## Define distractor feature matrix.
+X3 = distractors.copy()
+X3.to_csv(os.path.join(ROOT_DIR, 'designs', f'X3.csv'), index=False)
