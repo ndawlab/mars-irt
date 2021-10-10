ca94272a5cf6bbcf2f5dff0464a69971bd803198
szorowi1 <szorowi1@gmail.com>
2021-10-10 11:48:30 -0400

    validation v1.0 (9) - add infrequency items

--- app/static/js/surveys/surveys.js
+++ app/static/js/surveys/surveys.js
@@ -34,21 +34,21 @@ var nfc6 = {
     "Thinking is not my idea of fun.",
     "I would rather do something that requires little thought than something that is sure to challenge my thinking abilities.",
     "I really enjoy a task that involves coming up with new solutions to problems.",
-    "I would prefer a task that is intellectual, difficult, and important to one that is somewhat important but does not require much thought."
+    "I would prefer a task that is intellectual, difficult, and important to one that is important but does not require much thought.",
 
     // Infrequency item
-    "Worrying too much about the 1953 Olympics"
+    'Please select "uncertain" as your response for this statement.'
 
   ],
   scale: [
-    "Extremely uncharacteristic of me",
-    "Somewhat uncharacteristic of me",
+    "Extremely<br>uncharacteristic<br>of me",
+    "Somewhat<br>uncharacteristic<br>of me",
     "Uncertain",
-    "Somewhat characteristic of me",
-    "Extremely uncharacteristic of me"
+    "Somewhat<br>characteristic<br>of me",
+    "Extremely<br>uncharacteristic<br>of me"
   ],
   reverse: [
-    false, false, false, false, false, false, false
+    false, false, true, true, false, false, false
   ],
   instructions: 'For each of the statements below, please indicate whether or not the statement is characteristic of you or of what you believe.',
   survey_width: 950,
@@ -58,7 +58,7 @@ var nfc6 = {
   on_finish: function(data) {
 
     // Score response on infrequncy item.
-    const scores = [0,1,1,1];
+    const scores = [1,1,0,1,1];
     data.infrequency = scores[data.responses['Q07']];
 
   }
@@ -78,13 +78,13 @@ var neuroticism = {
 
     // Neuroticism beliefs (reverse-scored)
     "Rarely get irritated.",
-    "Seldom feel blue.",
+    "Rarely feel blue.",
     "Feel comfortable with myself.",
     "Am not easily bothered by things.",
-    "Am very pleased with myself."
+    "Am very pleased with myself.",
 
     // Infrequency item
-    "I am usually able to remember my own name."
+    "Can remember my own name."
 
   ],
   scale: [
@@ -97,15 +97,16 @@ var neuroticism = {
   reverse: [
     false, false, false, false, false, true, true, true, true, true, false
   ],
-  instructions: 'Select the option that best describes how typical or characteristic each item is of you.',
+  instructions: "The following are some phrases describing people's behaviors. Please use the rating scale to describe how accurately each statement describes you.",
+  scale_repeat: 6,
   survey_width: 950,
   item_width: 40,
-  infrequency_items: [8],
+  infrequency_items: [10],
   data: {survey: 'neuroticism'},
   on_finish: function(data) {
 
     // Score response on infrequncy item.
-    const scores = [1,1,1,0.5,0];
+    const scores = [1,1,1,0,0];
     data.infrequency = scores[data.responses['Q11']];
 
   }
@@ -129,10 +130,10 @@ var openness = {
     "Do not like art.",
     "Avoid philosophical discussions.",
     "Do not enjoy going to art museums.",
-    "Tend to vote for conservative political candidates."
+    "Tend to vote for conservative political candidates.",
 
     // Infrequency item
-    "Believe that I need food and water to stay alive."
+    "Can live for months without oxygen."
 
   ],
   scale: [
@@ -145,16 +146,16 @@ var openness = {
   reverse: [
     false, false, false, true, true, true, true, true, true, true, false
   ],
-  instructions: "The following are phrases that describe people's behaviors. Indicate how accurately each statement describes you. Describe yourself as you generally are now, not as you wish to be in the future.",
+  instructions: "The following are some phrases describing people's behaviors. Please use the rating scale to describe how accurately each statement describes you.",
   scale_repeat: 6,
-  survey_width: 900,
+  survey_width: 950,
   item_width: 40,
   infrequency_items: [10],
   data: {survey: 'openness'},
   on_finish: function(data) {
 
     // Score response on infrequncy item.
-    const scores = [1,1,0.5,0,0];
+    const scores = [0,0,1,1,1];
     data.infrequency = scores[data.responses['Q11']];
 
   }
@@ -194,3 +195,4 @@ var infrequency_check = {
 
 // Define survey block
 var SURVEYS = jsPsych.randomization.shuffle([nfc6, neuroticism, openness]);
+SURVEYS = SURVEYS.concat(infrequency_check)
