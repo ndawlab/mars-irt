c9304925a93ec0047b9634bf054b725945d75816
szorowi1 <szorowi1@gmail.com>
2021-11-16 16:12:57 -0500

    validation 2.0 (1) - add new short forms

---
+++ app/static/js/mars/mars-short-forms.js
@@ -0,0 +1,51 @@
+//---------------------------------------//
+// Define puzzle sets.
+//---------------------------------------//
+
+// Test form 1
+var test_form_1 = [
+  {item: 55, distractor: 'pd', shape_set: 2, 'form': 1},
+  {item: 66, distractor: 'pd', shape_set: 1, 'form': 1},
+  {item: 30, distractor: 'pd', shape_set: 1, 'form': 1},
+  {item: 58, distractor: 'md', shape_set: 2, 'form': 1},
+  {item: 45, distractor: 'pd', shape_set: 3, 'form': 1},
+  {item: 29, distractor: 'pd', shape_set: 3, 'form': 1},
+  {item: 17, distractor: 'pd', shape_set: 2, 'form': 1},
+  {item: 24, distractor: 'pd', shape_set: 3, 'form': 1},
+  {item: 72, distractor: 'pd', shape_set: 2, 'form': 1},
+  {item: 64, distractor: 'pd', shape_set: 3, 'form': 1},
+  {item: 76, distractor: 'pd', shape_set: 3, 'form': 1},
+  {item: 35, distractor: 'pd', shape_set: 3, 'form': 1}
+];
+
+// Test form 2
+var test_form_2 = [
+  {item: 45, distractor: 'pd', shape_set: 1, 'form': 2},
+  {item: 30, distractor: 'pd', shape_set: 2, 'form': 2},
+  {item: 55, distractor: 'pd', shape_set: 3, 'form': 2},
+  {item: 29, distractor: 'pd', shape_set: 1, 'form': 2},
+  {item: 64, distractor: 'pd', shape_set: 1, 'form': 2},
+  {item: 35, distractor: 'pd', shape_set: 1, 'form': 2},
+  {item: 24, distractor: 'pd', shape_set: 1, 'form': 2},
+  {item: 17, distractor: 'pd', shape_set: 3, 'form': 2},
+  {item: 58, distractor: 'md', shape_set: 3, 'form': 2},
+  {item: 76, distractor: 'pd', shape_set: 1, 'form': 2},
+  {item: 72, distractor: 'pd', shape_set: 3, 'form': 2},
+  {item: 66, distractor: 'pd', shape_set: 2, 'form': 2}
+];
+
+// Test form 3
+var test_form_3 = [
+  {item: 45, distractor: 'pd', shape_set: 2, 'form': 3},
+  {item: 55, distractor: 'pd', shape_set: 1, 'form': 3},
+  {item: 30, distractor: 'pd', shape_set: 3, 'form': 3},
+  {item: 58, distractor: 'md', shape_set: 1, 'form': 3},
+  {item: 17, distractor: 'pd', shape_set: 1, 'form': 3},
+  {item: 72, distractor: 'pd', shape_set: 1, 'form': 3},
+  {item: 29, distractor: 'pd', shape_set: 2, 'form': 3},
+  {item: 76, distractor: 'pd', shape_set: 2, 'form': 3},
+  {item: 35, distractor: 'pd', shape_set: 2, 'form': 3},
+  {item: 64, distractor: 'pd', shape_set: 2, 'form': 3},
+  {item: 66, distractor: 'pd', shape_set: 3, 'form': 3},
+  {item: 24, distractor: 'pd', shape_set: 2, 'form': 3}}
+]
