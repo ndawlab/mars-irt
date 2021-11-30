4d91cf5017aa0aae440944db32935ee4abe1c5e2
szorowi1 <szorowi1@gmail.com>
2021-11-30 15:00:35 -0500

    validation day 1 collection

--- 04_Validation/scripts/collate_data.py
+++ 04_Validation/scripts/collate_data.py
@@ -110,3 +110,87 @@ SURVEYS.insert(5, 'bot', SURVEYS.filter(regex='bot').sum(axis=1))
 
 ## Save data.
 SURVEYS.to_csv(os.path.join(DATA_DIR, 'surveys.csv'), index=False)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+### Assemble MARS data.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+## Locate files.
+files = sorted([f for f in os.listdir(RAW_DIR) if f.endswith('mars.json')])
+
+## Main loop.
+MARS = []
+for f in files:
+        
+    ## Define subject
+    subject = f.replace('_mars.json','')
+    
+    ## Load JSON.
+    with open(os.path.join(RAW_DIR, f), 'r') as tmp:
+        JSON = json.load(tmp)
+        
+    ## Extract MARS task.
+    mars = [dd for dd in JSON if dd['trial_type'] == 'mars']
+    mars = DataFrame(mars).query('item_set != "practice"').reset_index(drop=True)
+    
+    ## Reduce to columns of interest.
+    cols = ['item_set','short_form','item','distractor','shape_set','choice','accuracy','rt',
+            'all_loaded','minimum_resolution','browser_interactions']
+    mars = mars[cols]
+    
+    ## Format columns.
+    mars['rt'] = np.round(mars['rt'] * 1e-3, 3)
+    mars['short_form'] = mars['short_form'].astype(int)
+    mars['all_loaded'] = mars['all_loaded'].astype(int)
+    mars.insert(0, 'subject', subject)
+    mars.insert(1, 'trial', np.arange(mars.shape[0])+1)
+    
+    ## Append.
+    MARS.append(mars)
+    
+## Concatenate and save data.
+MARS = concat(MARS).sort_values(['subject','trial'])
+MARS.to_csv(os.path.join(DATA_DIR, 'mars.csv'), index=False)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+### Assemble RPM data.
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+## Locate files.
+files = sorted([f for f in os.listdir(RAW_DIR) if f.endswith('rpm.json')])
+
+## Main loop.
+RPM = []
+for f in files:
+        
+    ## Define subject
+    subject = f.replace('_rpm.json','')
+    
+    ## Load JSON.
+    with open(os.path.join(RAW_DIR, f), 'r') as tmp:
+        JSON = json.load(tmp)
+        
+    ## Extract RPM task.
+    rpm = [dd for dd in JSON if dd['trial_type'] == 'rpm']
+    rpm = DataFrame(rpm)
+    
+    ## Reduce to columns of interest.
+    cols = ['test_form','stimulus','correct','choice','accuracy','rt',
+            'all_loaded','minimum_resolution']
+    rpm = rpm[cols]
+    
+    ## Format columns.
+    rpm['test_form'] = rpm['test_form'].replace({'a':0, 'b':1})
+    try: rpm['stimulus'] = [s.split('/')[-1].replace('.png','') for s in rpm.stimulus]
+    except: raise ValueError(f) 
+    rpm['rt'] = np.round(rpm['rt'] * 1e-3, 3)
+    rpm['all_loaded'] = rpm['all_loaded'].astype(int)
+    rpm.insert(0, 'subject', subject)
+    rpm.insert(1, 'trial', np.arange(rpm.shape[0])+1)
+    
+    ## Append.
+    RPM.append(rpm)
+    
+## Concatenate and save data.
+RPM = concat(RPM).sort_values(['subject','trial'])
+RPM.to_csv(os.path.join(DATA_DIR, 'rpm.csv'), index=False)
